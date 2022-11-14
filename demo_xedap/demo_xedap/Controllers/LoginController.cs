7078800fdd4bcc5f37ec90eca51b99e8261218cb
DucAnh <ducanhmac113@gmail.com>
2022-11-15 01:14:58 +0700

    xong login

---
+++ demo_xedap/demo_xedap/Controllers/LoginController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using demo_xedap.Models;
+using System.Data.Entity;
+
+
+namespace WebBanVali.Controllers
+{
+    public class LoginController : Controller
+    {
+        DBWebBanXeDapEntities db = new DBWebBanXeDapEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DangNhap(tMembership user)
+        {
+            var user2 = db.tMemberships.Where(n => n.TaiKhoan.Equals(user.TaiKhoan) && n.MatKhau.Equals(user.MatKhau)).SingleOrDefault();
+            if (user2 == null)
+            {
+                ViewBag.message = "Sai ten nguoi dung hoac mat khau";
+                return RedirectToAction("Index", "Login");
+            }
+            else
+            {
+                Session["userName"] = user.TaiKhoan;
+                return RedirectToAction("Index", "Home");
+            }
+        }
+        [HttpGet]
+        public ActionResult Dangky()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult Dangky(tMembership taikhoan1)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tMemberships.Add(taikhoan1);
+                db.SaveChanges();
+                return View("Index");
+            }
+            return View(taikhoan1);
+        }
+    }
+}
--- demo_xedap/demo_xedap/Models/Model1.Designer.cs
+++ demo_xedap/demo_xedap/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Long\OneDrive\Máy tính\git\btl-Web\demo_xedap\demo_xedap\Models\BanXeDbContext.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\btl-Web\demo_xedap\demo_xedap\Models\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
